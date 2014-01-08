0eec094a93a31c88d0a19e0c7a5807a9c067fa68
Georgi Baghdasaryan <baghdasaryan@ucla.edu>
2014-01-08 00:05:10 -0800

    finished second query

--- project_1/part_b/queries.sql
+++ project_1/part_b/queries.sql
@@ -1,14 +1,10 @@
 -- Contains SELECT statements that query the dataset
 
-SELECT * FROM tbl;
+SELECT * FROM tbl ... WHERE 'Die Another Day';
 
 -- Give me the names of all the actors in the movie 'Die Another Day'. Please also make sure actor names are in this format:  <firstname> <lastname>   (seperated by single space)
 
 
-
--- Give me the count of all the actors who acted in multiple movies.
-SELECT COUNT(*) FROM MovieActor WHERE ; -- Use group by and then count to select all the actots with > 1 movies
-
-
-
+-- There might be a more efficient way to do this.....
+SELECT COUNT(*) FROM (SELECT aid, COUNT(mid) AS numMovies FROM MovieActor GROUP BY aid) AS MoviesPerActor WHERE MoviesPerActor.numMovies > 1;
 
