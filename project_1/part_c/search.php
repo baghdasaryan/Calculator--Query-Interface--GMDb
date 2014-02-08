e5dde2ee736c7e1117b640c78694619ad4f42be2
Georgi Baghdasaryan <baghdasaryan@ucla.edu>
2014-02-08 02:39:19 -0800

    better search results for empty queries and queries with empty result set

--- project_1/part_c/search.php
+++ project_1/part_c/search.php
@@ -26,28 +26,39 @@
       if(!empty($_GET)) {
         $qResult = dbSearch($_GET);
         if($qResult["data"]) {
-          echo '<h2>Search Results:</h2>';
+          echo '<h2>Search Results:</h2>' . PHP_EOL;
 
           $actors = $qResult["data"]["actors"];
           $movies = $qResult["data"]["movies"];
 
-          // Construct table with results
-          echo '<table border="1">' . PHP_EOL;
-          echo '<tr><th>Actors</th><th>Movies</th></tr>' . PHP_EOL;
-          echo '<tr><td valign="top">' . PHP_EOL;
+          if(mysql_num_rows($actors) == 0 || mysql_num_rows($movies) == 0) {
+            echo 'Your search - <b>' . $_GET["searchInput"] . '</b> - did not match any data.' . PHP_EOL;
+            echo 'Suggestions:' . PHP_EOL;
+            echo '<ul>' . PHP_EOL;
+            echo '<li>Make sure all words are spelled correctly.</li>' . PHP_EOL;
+            echo '<li>Try different keywords.</li>' . PHP_EOL;
+            echo '<li>Try fewer keywords.</li>' . PHP_EOL;
+            echo '</ul>' . PHP_EOL;
+          } else {
 
-          while($row = mysql_fetch_assoc($actors)) {
-            echo '<a href=showActorInfo.php?id=' . $row['id'] . '>' . $row['name'] . ' (' . $row['dob'] . ')' . '</a><br />' . PHP_EOL;
+            // Construct table with results
+            echo '<table border="1">' . PHP_EOL;
+            echo '<tr><th>Actors</th><th>Movies</th></tr>' . PHP_EOL;
+            echo '<tr><td valign="top">' . PHP_EOL;
+
+            while($row = mysql_fetch_assoc($actors)) {
+              echo '<a href=showActorInfo.php?id=' . $row['id'] . '>' . $row['name'] . ' (' . $row['dob'] . ')' . '</a><br />' . PHP_EOL;
+            }
+
+            echo '</td><td valign="top">' . PHP_EOL;
+
+            while($row = mysql_fetch_assoc($movies)) {
+              echo '<a href=showMovieInfo.php?id=' . $row['id'] . '>' . $row['title'] . '</a><br />' . PHP_EOL;
+            }
+
+            echo '</td></tr>' . PHP_EOL;
+            echo '</table>' . PHP_EOL;
           }
-
-          echo '</td><td valign="top">' . PHP_EOL;
-
-          while($row = mysql_fetch_assoc($movies)) {
-            echo '<a href=showMovieInfo.php?id=' . $row['id'] . '>' . $row['title'] . '</a><br />' . PHP_EOL;
-          }
-
-          echo '</td></tr>' . PHP_EOL;
-          echo '</table>' . PHP_EOL;
         } else {
           if(!empty($qResult["err"])) {
             $errors[] = $qResult["err"];
